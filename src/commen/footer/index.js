f5bcf7d3d2d9c2c34260027ea7c0184a0942e213
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-08-29 12:06:04 +0530

    last changes

--- src/commen/footer/index.js
+++ src/commen/footer/index.js
@@ -4,13 +4,13 @@ import { NavLink, useLocation } from 'react-router-dom';
 import FooterLogo from '../../assets/images/svg/footerLogo';
 
 export default function Footer() {
-  // const location = useLocation();
+  const location = useLocation();
 
-  // const isSpecialPage = location.pathname === '/donate' || location.pathname === '/auss2024' || location.pathname === '/lwandle';
+  const isSpecialPage = location.pathname === '/donate' || location.pathname === '/auss2024' || location.pathname === '/lwandle';
 
   return (
     <>
-      <footer>
+      <footer className={isSpecialPage ? 'footer special-footer' : 'footer'}>
         <div className="footer-links">
           <NavLink to={"/impressum"}>IMPRESSUM</NavLink>
           <NavLink to={"/datenschutz"}>DATENSCHUTZ</NavLink>
