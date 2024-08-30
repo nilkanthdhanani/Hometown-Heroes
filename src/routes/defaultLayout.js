de2168f536e02060075069498b20173a0377066f
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-08-30 17:47:07 +0530

    last changes

--- src/routes/defaultLayout.js
+++ src/routes/defaultLayout.js
@@ -4,6 +4,19 @@ import Header from '../commen/header';
 import Footer from '../commen/footer';
 
 export default function DefaultLayout() {
+  const font = new FontFace(
+    'Big Shoulders Display',
+    'url(https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@100..900&family=Big+Shoulders+Inline+Text:wght@100..900&display=swap)'
+  );
+
+  font.load().then(function (loadedFont) {
+    document.fonts.add(loadedFont);
+
+    document.body.style.fontFamily = '"Big Shoulders Display", sans-serif';
+  }).catch(function (error) {
+    console.error('Font failed to load:', error);
+  });
+
   const location = useLocation();
 
   const specialPages = ['/donate', '/auss2024', '/lwandle'];
