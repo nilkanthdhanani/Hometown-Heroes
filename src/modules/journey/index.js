0057dc5539aa2c989a5b1ac007d8b9528f316f7a
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-09-04 17:46:33 +0530

    journey mobile

--- src/modules/journey/index.js
+++ src/modules/journey/index.js
@@ -7,18 +7,20 @@ export default function Journey() {
     return (
         <>
             <div className="journey">
-                <div className="containerH">
-                    <div className="journey-banner">
-                        <div className="journey-head">
-                            <h1>FOLLOW THE JOURNEY</h1>
-                        </div>
+                <div className="journey-banner">
+                    <div className="containerH">
                         <div className="journey-banner-div">
-                            <div className="journey-banner-div-intro">
-                                <h2>INTRO</h2>
-                                <p>“Welcome to this comprehensive document that encapsulates the essence and journey of HOMETOWN HEROES. Within this page, you will find a vivid portrayal of our mission, our experiences, and our unwavering commitment to driving positive change. Explore how HOMETOWN HEROES, with its roots in the townships of Cape Town, will blossom into a platform of compassion, artistry, and impactful collaborations.“</p>
+                            <div className="journey-head">
+                                <h1>FOLLOW THE JOURNEY</h1>
                             </div>
-                            <div className="journey-banner-img">
-                                <img src={journey} alt="journey" />
+                            <div className="journey-banner-div">
+                                <div className="journey-banner-div-intro">
+                                    <h2>INTRO</h2>
+                                    <p>“Welcome to this comprehensive document that encapsulates the essence and journey of HOMETOWN HEROES. Within this page, you will find a vivid portrayal of our mission, our experiences, and our unwavering commitment to driving positive change. Explore how HOMETOWN HEROES, with its roots in the townships of Cape Town, will blossom into a platform of compassion, artistry, and impactful collaborations.“</p>
+                                </div>
+                                <div className="journey-banner-img">
+                                    <img src={journey} alt="journey" />
+                                </div>
                             </div>
                         </div>
                     </div>
