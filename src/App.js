9c0dbf2da2d653b5174bbbb8045ec89ddb9de55e
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-09-20 11:08:20 +0530

    error change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './routes';
-import './styles/index.scss';
+// import './styles/index.scss';
 
 function App() {
   return (
